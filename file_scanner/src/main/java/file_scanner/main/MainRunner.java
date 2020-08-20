baffb6576a9e1ddb1792dcb116632bd14b7a61fb
ybaranov <baranovy@ukr.net>
2020-08-20 13:50:07 +0300

    developed.

--- file_scanner/src/main/java/file_scanner/main/MainRunner.java
+++ file_scanner/src/main/java/file_scanner/main/MainRunner.java
@@ -1,10 +1,26 @@
 package file_scanner.main;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import file_scanner.process.RootProcessor;
+
 public class MainRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if (args.length != 1) {
+			System.out.println("Application should have only one argument which is the file or catalog path.");
+			return;
+		}
+		
+		Path rootPath = Paths.get(args[0]);
+		File root = rootPath.toFile();
+		if (!root.exists()) {
+			System.out.println("Root file doesn't exist.");
+			return;
+		}
+		new RootProcessor().processRoot(rootPath, root);
 	}
-
+	
 }
